cb1b9d7568d7b92afc480dc36250e5b456fd30d7
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-19 15:47:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def long_planeteer_calls(array)
   array.any? { |arr| arr.size > 4}
 end
 
-def find_the_cheese(menu_items)
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |type|
     if arr.include?(type)
